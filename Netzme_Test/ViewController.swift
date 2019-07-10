cc5c55b358e98725c80c0715959b3ea6e5abf169
Ernando Kasaluhe <ernandokasaluhe@Ernandos-MacBook-Pro.local>
2019-07-10 16:01:39 +0700

    Netzme test first commit

---
+++ Netzme_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Netzme_Test
+//
+//  Created by Ernando Kasaluhe on 10/07/19.
+//  Copyright © 2019 Ernando Kasaluhe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
